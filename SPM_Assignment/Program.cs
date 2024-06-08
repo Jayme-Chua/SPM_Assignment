734d9d74ccb0012254ac4b608b62f4a3457c9625
josephwan21 <josephbwanzj@gmail.com>
2024-06-09 00:54:30 +0800

    Create and display board, coins and score

--- SPM_Assignment/Program.cs
+++ SPM_Assignment/Program.cs
@@ -1,27 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//////For board 1
+////Console.WriteLine("     A   B   C   D   E   F   G   H   I   J  ");
+////Console.WriteLine("   +---+---+---+---+---+---+---+---+---+---+");
 
-//For board 1
-Console.WriteLine("     A   B   C   D   E   F   G   H   I   J  ");
-Console.WriteLine("   +---+---+---+---+---+---+---+---+---+---+");
+//Board1 = {
+//            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+//            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+//        };
 
-/*Board1 = { 
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
-        };
-*/
-//For board 2   
+
+/*For board 2   
 Console.WriteLine("     A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T");
 Console.WriteLine("   +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+");
-
+*/
 /*Board2 = {
             { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
             { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
@@ -48,10 +46,28 @@ Console.WriteLine("   +---+---+---+---+---+---+---+---+---+---+---+---+---+---+-
 */
 //Commented out the board because it created 1000 errors
 
+
+using System.Reflection.Metadata.Ecma335;
+using System.Transactions;
+
+int coins = 0;
+int score = 0;
+
+
+
 while (true)
 {
     MainMenu();
-    int option = Convert.ToInt32(Console.ReadLine());
+
+    int option = -1; 
+    try
+    {
+        option = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid input format. Please enter a number.");
+    }
 
     if (option == 0)
     {
@@ -61,34 +77,57 @@ while (true)
 
     else if (option == 1)
     {
-        GameMenu();
-
-        int gameOption = Convert.ToInt32(Console.ReadLine());
-
-        if (gameOption == 0)
+        while (true)
         {
-            Console.WriteLine("Quitting game. Thanks for playing!");
-            return;
-        }
+            GameMenu();
 
-        else if (gameOption == 1)
-        {
-            Console.WriteLine("Starting Arcade Mode...");
-        }
-        else if (gameOption == 2)
-        {
-            Console.WriteLine("Starting Free Play Mode...");
-        }
-        else if (gameOption == 3)
-        {
-            Console.WriteLine("Returning to Main Menu");
-            break;
-        }
+            int gameOption = -1;
 
+            try
+            {
+                gameOption = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input format. Please enter a number.");
+            }
+
+            if (gameOption == 0)
+            {
+                Console.WriteLine("Quitting game. Thanks for playing!");
+                return;
+            }
+
+            else if (gameOption == 1)
+            {
+                Console.WriteLine("Starting Arcade Mode...");
+                ArcadeBoard(coins, score);
+                Console.WriteLine("Press [0] to exit.");
+            }
+            else if (gameOption == 2)
+            {
+                Console.WriteLine("Starting Free Play Mode...");
+                FreePlayBoard(score);
+            }
+            else if (gameOption == 3)
+            {
+                Console.WriteLine("Returning to Main Menu");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option.");
+                continue;
+            }
+        }
     }
     else if (option == 2)
     {
         Console.WriteLine("Loading game...");
+    }
+    else if (option == 3)
+    {
+        Console.WriteLine("Viewing High Scores...");
     }
     else
     {
@@ -98,19 +137,137 @@ while (true)
 
 void MainMenu()
 {
+    Console.Clear();
     Console.WriteLine("Welcome to Ngee Ann City! Indulge in the creation of a happy and prosperous city!");
     Console.WriteLine("---------------------------------------------------------------------------------");
     Console.WriteLine("[1] New Game");
     Console.WriteLine("[2] Load Game");
+    Console.WriteLine("[3] View High Scores");
     Console.WriteLine("[0] Quit Game");
 }
 
 void GameMenu()
 {
+    Console.Clear();
     Console.WriteLine("Choose your game mode: Arcade or Free Play");
     Console.WriteLine("------------------------------------------");
     Console.WriteLine("[1] Arcade Mode");
     Console.WriteLine("[2] Free Play Mode");
     Console.WriteLine("[3] Back to Main Menu");
     Console.WriteLine("[0] Quit Game");
+}
+
+void ArcadeBoard(int coins, int score)
+{
+    Console.Clear();
+    int rows = 20;
+    int cols = 20;
+
+    char[,] board = new char[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            board[i, j] = ' ';
+        }
+    }
+
+    DisplayGrid(board);
+
+    Console.WriteLine("\nCoins: " + coins + "\tScore: " + score);
+
+    Console.WriteLine("Press [0] to exit to the Main Menu.");
+
+    while (true)
+    {
+        string input = Console.ReadLine();
+        if (input == "0")
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Try again.");
+        }
+    }
+}
+
+
+void FreePlayBoard(int score)
+{
+    Console.Clear();
+    int rows = 5;
+    int cols = 5;
+    int expansionSize = 5;
+
+
+    char[,] board = new char[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            board[i, j] = ' ';
+        }
+    }
+
+    // Display the initial city grid
+    DisplayGrid(board);
+
+    Console.WriteLine("\nCoins: Unlimited" + "\tScore: " + score);
+    Console.WriteLine("Press [0] to exit to the Main Menu.");
+    
+
+    while (true)
+    {
+        string input = Console.ReadLine();
+
+        if (input == "0")
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Try again.");
+        }
+    }
+}
+
+void DisplayGrid(char[,] board)
+{
+    int rows = board.GetLength(0);
+    int cols = board.GetLength(1);
+
+    Console.Write("     ");
+    for (int c = 0; c < cols; c++)
+    {
+        Console.Write(" " + (char)('A' + c) + "  "); //Letters move to a new letter across the board horizontally
+    }
+    Console.WriteLine();
+
+    Console.Write("    ");
+    for (int c = 0; c < cols; c++)
+    {
+        Console.Write("+---");
+    }
+    Console.WriteLine("+");
+
+    for (int r = 0; r < rows; r++)
+    {
+        Console.Write(" " + (r + 1).ToString("D2") + " ");
+
+        for (int c = 0; c < cols; c++)
+        {
+            Console.Write("| " + board[r, c] + " ");
+        }
+        Console.WriteLine("|");
+
+        Console.Write("    ");
+        for (int c = 0; c < cols; c++)
+        {
+            Console.Write("+---");
+        }
+        Console.WriteLine("+");
+    }
 }
